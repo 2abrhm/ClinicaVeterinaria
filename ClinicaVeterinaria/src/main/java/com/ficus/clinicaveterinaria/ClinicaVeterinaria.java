6b3c9547e641e2254dc9a67720827b41a898b4d1
2abrhm <adelahozp@unicartagena.edu.co>
2026-09-08 18:26:05 -0500

    Primer commit

---
+++ ClinicaVeterinaria/src/main/java/com/ficus/clinicaveterinaria/ClinicaVeterinaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ficus.clinicaveterinaria;
+
+/**
+ *
+ * @author abrah
+ */
+public class ClinicaVeterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
